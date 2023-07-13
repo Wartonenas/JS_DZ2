b605f1dbe912f07499ba61d4217a7d04ba9bf752
Zhukov Ilya <136591141+Wartonenas@users.noreply.github.com>
2023-07-13 15:52:54 +0600

    Add files via upload

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -21,11 +21,50 @@ let positionX = 0
 let positionY = 0
 
 const move = () => {
-	if (positionX <= 446 & positionY === 0) {
+	if (positionX < 446 && positionY === 0) {
 		positionX += 1
 		childBlock.style.left = `${positionX}px`
+		setTimeout(move), 1
+	} else if (positionX === 446 && positionY < 446) {
+		positionY += 1
+		childBlock.style.top = `${positionY}px`
+		setTimeout(move), 1
+	} else if (positionX > 0 && positionY === 446) {
+		positionX -= 1
+		childBlock.style.left = `${positionX}px`
+		setTimeout(move), 1
+	} else if (positionX === 0 && positionY > 0) {
+		positionY -= 1
+		childBlock.style.top = `${positionY}px`
 		setTimeout(move), 1
 	}
 }
 
 move()
+
+// STOPWATH
+const start = document.querySelector('#start')
+const stop = document.querySelector('#stop')
+const reset = document.querySelector('#reset')
+const seconds = document.querySelector('#seconds')
+
+let second = 0
+let interval
+
+function startTimer() {
+	second++
+	seconds.innerHTML = `${second}`
+}
+
+start.onclick = () => {
+	clearInterval(interval)
+	interval = setInterval(startTimer, 1000)
+}
+stop.onclick = () => {
+	clearInterval(interval)
+}
+reset.onclick = () => {
+	clearInterval(interval)
+	seconds.innerHTML = 0
+	second = 0
+}
