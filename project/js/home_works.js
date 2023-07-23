0bb0393f2a4cbcd5db3d8e926915a8e5f299a00c
Zhukov Ilya <136591141+Wartonenas@users.noreply.github.com>
2023-07-23 10:53:21 +0600

    Add files via upload

--- project/js/home_works.js
+++ project/js/home_works.js
@@ -6,10 +6,10 @@ const regExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]
 
 gmailButton.onclick = () => {
 	if (regExp.test(gmailInput.value)) {
-		gimalResult.innerHTML = 'OK' 
+		gimalResult.innerHTML = 'OK'
 		gimalResult.style.color = 'green'
 	} else {
-		gimalResult.innerHTML = 'NOT OK' 
+		gimalResult.innerHTML = 'NOT OK'
 		gimalResult.style.color = 'red'
 	}
 }
@@ -81,3 +81,60 @@ request.addEventListener('load', () => {
 	});
 })
 
+// Convertot
+
+const som = document.querySelector('#som')
+const usd = document.querySelector('#usd')
+const euro = document.querySelector('#euro')
+const yuan = document.querySelector('#yuan')
+const yen = document.querySelector('#yen')
+
+const convert = (input, conc1, conc2, conc3, conc4) => {
+	input.oninput = () => {
+		const request = new XMLHttpRequest()
+		request.open('GET', '../json/convertor.json')
+		request.setRequestHeader('Content-type', 'application/json')
+		request.send()
+		request.onload = () => {
+			const response = JSON.parse(request.response)
+			if (input === som) {
+				conc1.value = (input.value * response.somUsd).toFixed(4);
+				conc2.value = (input.value * response.somEuro).toFixed(4)
+				conc3.value = (input.value * response.somYuan).toFixed(4)
+				conc4.value = (input.value * response.somYen).toFixed(4)
+			} else if (input === usd) {
+				conc1.value = (input.value * response.usdSom).toFixed(4);
+				conc2.value = (input.value * response.usdEuro).toFixed(4)
+				conc3.value = (input.value * response.usdYuan).toFixed(4)
+				conc4.value = (input.value * response.usdYen).toFixed(4)
+			} else if (input === euro) {
+				conc1.value = (input.value * response.euroSom).toFixed(4);
+				conc2.value = (input.value * response.euroUsd).toFixed(4)
+				conc3.value = (input.value * response.euroYuan).toFixed(4)
+				conc4.value = (input.value * response.euroYen).toFixed(4)
+			} else if (input === yuan) {
+				conc1.value = (input.value * response.yuanSom).toFixed(4);
+				conc2.value = (input.value * response.yuanUsd).toFixed(4)
+				conc3.value = (input.value * response.yuanEuro).toFixed(4)
+				conc4.value = (input.value * response.yuanYen).toFixed(4)
+			} else if (input === yen) {
+				conc1.value = (input.value * response.yenSom).toFixed(4);
+				conc2.value = (input.value * response.yenUsd).toFixed(4)
+				conc3.value = (input.value * response.yenEuro).toFixed(4)
+				conc4.value = (input.value * response.yenYuan).toFixed(4)
+			}
+			if (input.value === '') {
+				conc1.value = ''
+				conc2.value = ''
+				conc3.value = ''
+				conc4.value = ''
+			}
+		}
+	}
+}
+
+convert(som, usd, euro, yuan, yen)
+convert(usd, som, euro, yuan, yen)
+convert(euro, som, usd, yuan, yen)
+convert(yuan, som, usd, euro, yen)
+convert(yen, som, usd, euro, yuan)
--- project/js/modal.js
+++ project/js/modal.js
@@ -16,16 +16,40 @@ setTimeout(openModal, 10000)
 
 let ModalOpen = false;
 window.addEventListener('scroll', () => {
-const scrollPosition = window.scrollY;
-const documentHeight = window.innerHeight;
-const clientHeight = document.body.clientHeight;
-const bottomThreshold = clientHeight - documentHeight - 0;
-if (scrollPosition >= bottomThreshold && !ModalOpen) {
-   openModal();
-   ModalOpen = true;
-}
+	const scrollPosition = window.scrollY;
+	const documentHeight = window.innerHeight;
+	const clientHeight = document.body.clientHeight;
+	const bottomThreshold = clientHeight - documentHeight - 0;
+	if (scrollPosition >= bottomThreshold && !ModalOpen) {
+		openModal();
+		ModalOpen = true;
+	}
 });
 
 modalTrigger.onclick = () => openModal()
 closeModal.onclick = () => closeModalBtn()
 modal.onclick = (event) => event.target === modal && closeModalBtn()
+
+// Post Data
+
+const form = document.querySelector('form')
+
+const postData = () => {
+	form.addEventListener('submit', (event) => {
+		event.preventDefault()
+		const request = new XMLHttpRequest()
+		request.open("POST", "server.php")
+		request.setRequestHeader('Content-type', 'application/json')
+
+		const formData = new formData(form)
+		const obj = {}
+		formData.forEach(item, i => {
+			obj[i] = item
+		});
+		const json = 
+
+		request.send()
+	})
+}
+
+postData(form)
